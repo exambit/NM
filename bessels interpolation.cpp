eaa4fa576186881be2ceb40bf4148e69ecee3ca4
exambit <157822377+exambit@users.noreply.github.com>
2024-04-17 15:53:39 +0530

    Update bessels interpolation.cpp

--- bessels interpolation.cpp
+++ bessels interpolation.cpp
@@ -1,81 +1,71 @@
-#include<stdio.h>
-#include<conio.h>
-#include<math.h>
-#include<process.h>
-#include<conio.h>
+#include <stdio.h>
+#include <math.h>
 
-int main()
-{
-int n;                          // no. of terms.
-int i,j;                        // Loop variables
-float ax[10];                   // 'X' array limit 9
-float ay[10];                   // 'Y' array limit 9
-float x;                        // User Query for what value of X
-float y;                      // Calculated value for coressponding X.
-float h;                        // Calc. Section
-float p;                        // Calc. Section
-float diff[20][20];             // to store Y
-float y1,y2,y3,y4;              // Formulae variables.
+#define MAX_SIZE 10
 
+float besselsInterpolation(float x, float ax[], float ay[], int n) {
+    float diff[MAX_SIZE][MAX_SIZE];
+    float h = ax[1] - ax[0];
+    float p = (x - ax[0]) / h;
+    float y = 0;
 
-printf("\t\t !! BESSELS INTERPOLATION FORMULA !! ");
-// Input section.
-printf("\n\n Enter the no. of terms -> ");
-scanf("%d",&n);
+    // Calculate first order differences
+    for (int i = 0; i < n; i++) {
+        diff[i][1] = ay[i];
+    }
 
-// Input Sequel for array X
-printf("\n\n Enter the value in the form of x -> ");
-// Input loop for X.
-for(i=0;i<n;i++)
-   {
-   printf("\n Enter the value of x%d -> ",i+1);
-   scanf("%f",&ax[i]);
-   }
+    // Calculate higher order differences
+    for (int j = 2; j <= n; j++) {
+        for (int i = 0; i < n - j + 1; i++) {
+            diff[i][j] = diff[i + 1][j - 1] - diff[i][j - 1];
+        }
+    }
 
-// Input sequel for array Y.
-printf("\n\n Enter the value in the form of y -> ");
-// Input loop for Y.
-for(i=0;i<n;i++)
-   {
-   printf("\n Enter the value of y%d -> ",i+1);
-   scanf("%f",&ay[i]);
-   }
+    // Perform interpolation
+    y += diff[0][1];
+    float term = 1;
+    for (int j = 1; j < n; j++) {
+        term *= (p - j + 1) / j;
+        y += term * diff[0][j + 1];
+    }
 
-// Inputting the required value quarry
-printf("\n\n Enter the value of x for ");
-printf("\n which u want the value of y -> ");
-scanf("%f",&x);
+    return y;
+}
 
-// Calculation and processing section.
-h=ax[1]-ax[0];
-for(i=0;i<n-1;i++)
-   diff[i][1]=ay[i+1]-ay[i];
+int main() {
+    printf("\t\t !! BESSELS INTERPOLATION FORMULA !!\n\n");
 
-for(j=2;j<=4;j++)
-   for(i=0;i<n-j;i++)
-      diff[i][j]=diff[i+1][j-1]-diff[i][j-1];
+    int n;
+    printf("Enter the number of terms -> ");
+    scanf("%d", &n);
 
-i=0;
-do
- {
- i++;
- }
-while(ax[i]<x);
+    if (n > MAX_SIZE) {
+        printf("Number of terms exceeds maximum limit.\n");
+        return 1;
+    }
 
-i--;
-p=(x-ax[i])/h;
-y1=p*diff[i][1];
-y2=p*(p-1)*(diff[i][2]+diff[i-1][2])/4;
-y3=p*(p-1)*(p-0.5)*diff[i-1][3]/6;
-y4=(p+1)*p*(p-1)*(p-2)*(diff[i-2][4]+diff[i-1][4])/48;
+    float ax[MAX_SIZE];
+    float ay[MAX_SIZE];
 
-// Taking sum
-y=ay[i]+y1+y2+y3+y4;
+    printf("\nEnter the values of x ->\n");
+    for (int i = 0; i < n; i++) {
+        printf("x%d: ", i + 1);
+        scanf("%f", &ax[i]);
+    }
 
-// Outut Section
-printf("\n When x = %6.2f , y = %6.8f",x,y);
+    printf("\nEnter the values of y ->\n");
+    for (int i = 0; i < n; i++) {
+        printf("y%d: ", i + 1);
+        scanf("%f", &ay[i]);
+    }
 
-// Invoke user watch halt function
-printf("\n\n\n\t\t\t !! PRESS ENTER TO EXIT !! ");
-return 0;
+    float x;
+    printf("\nEnter the value of x for interpolation: ");
+    scanf("%f", &x);
+
+    float y = besselsInterpolation(x, ax, ay, n);
+
+    printf("\nInterpolated value of y at x = %.2f is: %.8f\n", x, y);
+
+    return 0;
 }
